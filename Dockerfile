95a93150338f76f381f9d29b3cdfae6411c8836f
Cherif Bouchelaghem <cherif@bitovi.com>
2024-08-27 16:02:23 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM dunglas/frankenphp
+
+RUN install-php-extensions \
+	pdo_mysql \
+	gd \
+	intl \
+	zip \
+	opcache
+
+COPY . /app
---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cherif\Shortenr;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
